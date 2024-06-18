ba153258bbc525f3e3c41da351eff080f59b88ab
Ilya Enkovich <ilya.enkovich@intel.com>
2024-06-17 19:00:14 -0500

    Fixes for x86 CI workflow (#26)
    * Fix RelWithDebInfo build.
    
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>
    
    * Skip fp8 cast tests on CPU.
    
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>
    
    * Fix segfault.
    
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>
    
    * [BACKEND] Update LLVM version to https://github.com/llvm/llvm-project/commit/765206e050453018e861637a08a4520f29238074 (#4059)
    
    * Add -s option to pytest run.
    
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>
    
    * Add a workaround for LLVM bug causing test failure on Skylake CPU.
    
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>
    
    * Add a workaround for LLVM fpext bug causing test failure on Skylake CPU.
    
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>
    
    * Fix formatting.
    
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>
    
    ---------
    
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>
    Co-authored-by: Pablo Zimmermann <karupayun@google.com>

--- python/src/ir.cc
+++ python/src/ir.cc
@@ -1679,6 +1679,8 @@ void init_triton_ir(py::module &&m) {
                           });
 
           ::llvm::DebugFlag = true;
+          // For release build setCurrentDebugTypes is a macro, so avoid
+          // namespace prefix
           using namespace llvm;
           setCurrentDebugTypes(debugTypes.data(), debugTypes.size());
         }
